cd6400065e2ab64c384634c19782db94ea5a0e1b
Aryan <garyan18@gmail.com>
2022-10-13 21:33:11 +0530

    Added picture paths

--- objects.py
+++ objects.py
@@ -4,7 +4,7 @@ import pygame, sys
 class Bird(pygame.sprite.Sprite):
     def __init__(self, bird_x,bird_y, bird_picture_path, bird_fx_path):
         super().__init__()
-        self.image
+        self.image=bird_picture_path
         self.img_rect = self.image.get_rect()
         self.img_rect.center = [bird_x, bird_y] 
         self.soundfx 
@@ -20,7 +20,7 @@ class Bird(pygame.sprite.Sprite):
 class Piggy(pygame.sprite.Sprite) :
     def __init__(self, pig_x,pig_y, pig_picture_path, pig_fx_path):
         super().__init__()
-        self.image 
+        self.image=pig_picture_path
         self.img_rect = self.image.get_rect()
         self.img_rect.center = [pig_x, pig_y] 
         self.soundfx
